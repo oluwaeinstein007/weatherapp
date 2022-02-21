ae1ae8bbe32f17cfbc0b9aea8fe1eed0b15afdcd
Sanni, Olanrewaju <slanre26@gmail.com>
2022-02-21 16:17:36 -0500

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+fetch("https://foreca-weather.p.rapidapi.com/location/search/mumbai?lang=en&country=in", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "foreca-weather.p.rapidapi.com",
+		"x-rapidapi-key": "ec71b998ebmshe861b618b344dabp11a79fjsn54b2dc78dd7d"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
